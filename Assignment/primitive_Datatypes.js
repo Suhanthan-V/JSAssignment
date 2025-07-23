2340982fb80f78a0eda7805706db856de742a052
Suhanthan-V <suhansmart10@gmail.com>
2025-07-23 13:13:52 +0530

    Adding files for Assignment

---
+++ Assignment/primitive_Datatypes.js
@@ -0,0 +1,22 @@
+const { log } = require("console")
+
+let firstName = "Suhan "
+let companyName = "Bankbazaar"
+let mobileNumber = 8637491124
+let isAutomation = true
+let hasPlaywright = Boolean
+
+console.log("First Name is :"+firstName);
+console.log(typeof firstName);
+
+console.log("Company Name is:"+companyName);
+console.log(typeof companyName);
+
+console.log("Mobile Number is :"+mobileNumber);
+console.log(typeof mobileNumber);
+
+console.log("is Automation :"+isAutomation);
+console.log(typeof isAutomation);
+
+console.log("hasPlaywright :"+hasPlaywright);
+console.log(typeof hasPlaywright);
