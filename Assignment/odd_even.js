61b668853632a0771dae5c4441e0db5228deb91d
Suhanthan-V <suhansmart10@gmail.com>
2025-07-25 21:28:52 +0530

    Assignment 2 Added

---
+++ Assignment/odd_even.js
@@ -0,0 +1,13 @@
+
+function isOddOrEven(number) {
+     if(number % 2 == 0){
+        console.log(`The Number ${number} is Even `);
+     }else {
+        console.log(`The Number ${number} is Odd `);
+     }
+    
+    }
+var numberOne = 25;
+var numberTwo = 10;
+isOddOrEven(numberOne)
+isOddOrEven(numberTwo)
